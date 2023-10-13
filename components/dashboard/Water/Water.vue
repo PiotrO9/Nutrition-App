99caad0017fbd7b0e1162f66c02606453db5ba80
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-10-13 20:15:06 +0200

    Add water section and structure for macros section

---
+++ components/dashboard/Water/Water.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="water">
+        <div class="wrapper">
+            <span class="water__title">
+                Woda
+            </span>
+            <div class="water__stats">
+                {{ currentater }} / {{ maxWater }} ml
+            </div>
+            <div class="water__glasses">
+                <NuxtImg src="water-glass.png" class="glass" />
+            </div>
+        </div>
+        <div class="add-glass">
+            <Icon class="plus" name="ic:baseline-plus" @click="addGlass" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+let currentater = 0;
+let maxWater = 2304;
+
+const addGlass = (): void => {
+
+}
+
+</script>
+
+<style scoped lang="scss">
+.water {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    padding-top: 20px;
+    width: 100%;
+    gap: 50px;
+
+    .wrapper {
+        width: 100%;
+        max-width: 250px;
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+    }
+
+    &__title {
+        @include font-style(20px, 700, $Inter);
+    }
+
+    &__stats {
+        color: $mediumGray;
+    }
+
+    &__glasses {
+        .glass {
+            @include square-size(40px);
+        }
+    }
+
+    .add-glass {
+        @include square-size(40px);
+        @include flex-center;
+        background-color: $seagreen;
+        border-radius: 50px;
+        cursor: pointer;
+
+        .plus {
+            @include square-size(30px);
+            color: $pure-white;
+        }
+    }
+}
+</style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,6 +3,7 @@
     <div class="wrapper">
         <main class="main">
             <Meals />
+            <Water />
         </main>
     </div>
     <DashboardFooter />
@@ -11,6 +12,7 @@
 <script setup lang="ts">
 import { useAuthStore } from '~/stores/auth';
 import Meals from '~/components/dashboard/Meals/Meals.vue';
+import Water from '~/components/dashboard/Water/Water.vue';
 
 const authStorage = useAuthStore();
 const user = useSupabaseUser();
@@ -30,12 +32,15 @@ watchEffect(() => {
 <style scoped lang="scss">
 .wrapper {
     @include flex-center;
+    height: calc(100% - 120px);
 }
 
 .main {
-    @include flex-center;
-    height: 100%;
-    width: 100%;
+    @include square-size(100%);
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
     max-width: 1024px;
 }
 </style>
